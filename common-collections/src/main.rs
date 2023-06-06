d6d273422ffb5f76a7a313cd32cb84e0b9ca1035
XMars <xtruongptit@gmail.com>
2023-06-06 10:45:50 +0700

    add pig latin

--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -1,4 +1,6 @@
+pub mod handle_str;
 pub mod handle_vector;
+use handle_str::pig_latin;
 use handle_vector::median;
 use handle_vector::mode;
 
@@ -6,4 +8,7 @@ fn main() {
     let a = vec![1, 1, 7, 4, 12, 9, 2, 6, 3];
     println!("{:?}", median::get_median(&a));
     println!("{:?}", mode::get_mode(&a));
+
+    let text = String::from("hi my old friend p");
+    println!("{}", pig_latin::convert_text_to_pig_latin(&text));
 }
