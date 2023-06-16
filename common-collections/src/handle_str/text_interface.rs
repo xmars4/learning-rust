92532c048d931351a176eaf4b342c2a4bb815c4e
XMars <xtruongptit@gmail.com>
2023-06-16 15:40:33 +0700

    finish text interface

--- common-collections/src/handle_str/text_interface.rs
+++ common-collections/src/handle_str/text_interface.rs
@@ -1,5 +1,5 @@
 use regex::{Regex, RegexBuilder};
-// use std::collections::HashMap;
+use std::collections::HashMap;
 
 fn remove_redundant_spaces(value: &str) -> String {
     let re = Regex::new(r"\s+").unwrap();
@@ -19,18 +19,35 @@ fn extract_valid_input(value: &str) -> Option<String> {
     }
 }
 
-fn extract_value(value: &str) -> &str {
-    let re = Regex::new(r"");
-    let part_with_data = &value[3..];
-    let to_location = Regex::new(r"to").unwrap().find(part_with_data).unwrap();
-    part_with_data
+pub fn extract_value(value: &str) -> Vec<&str> {
+    let to_location = RegexBuilder::new(r"to")
+        .case_insensitive(true)
+        .build()
+        .unwrap()
+        .find(value)
+        .unwrap();
+    // let to_location = Regex::new(r"to").unwrap().find(value).unwrap();
+    let start_to = to_location.start();
+    let employee_name = &value[4..start_to - 1];
+    let department_name = &value[start_to + 4..];
+    vec![employee_name, department_name]
 }
 
-pub fn add_employee_to_department(statement: &str) -> bool {
+pub fn add_employee_to_department(
+    statement: &str,
+    department_data: &mut HashMap<String, Vec<String>>,
+) -> bool {
     let valid_input = extract_valid_input(statement).unwrap_or(String::from("invalid"));
-    if valid_input != "invalid" {
-        false;
+    if valid_input == "invalid" {
+        return false;
     }
-    true
-    // check_valid_input(statement)
+    let new_data = extract_value(statement);
+    let employee_name = new_data[0].to_string();
+    let department_name = new_data[1].to_string();
+    if let Some(x) = department_data.get_mut(&department_name) {
+        x.push(employee_name);
+    } else {
+        department_data.insert(department_name, vec![employee_name]);
+    }
+    return true;
 }
--- common-collections/src/main.rs
+++ common-collections/src/main.rs
@@ -5,6 +5,7 @@ use handle_str::text_interface;
 use handle_vector::median;
 use handle_vector::mode;
 use regex::{Regex, RegexBuilder};
+use std::collections::HashMap;
 
 fn main() {
     // let a = vec![1, 1, 7, 4, 12, 9, 2, 6, 3];
@@ -13,13 +14,18 @@ fn main() {
 
     // let text = String::from("hi my old friend p");
     // println!("{}", pig_latin::convert_text_to_pig_latin(&text));
+    let mut department_data: HashMap<String, Vec<String>> = HashMap::new();
+    let statement = String::from("AdD pro to  sales");
+    let statement2 = String::from("Add dddd none to  BOD");
+    let statement3 = String::from("Add DEv-rust  TO  sales");
+    let incorrect_statement3 = String::from("Add DEv-rust  TaaO  sales");
 
-    let statement = String::from("AdD mex to y");
-    let mat = Regex::new(r"to").unwrap().find(&statement).unwrap();
-    let start_to = mat.start();
-    let employee_name = &statement[4..start_to - 1];
-    let department_name = &statement[start_to + 3..];
-    println!("{} {}", employee_name, department_name);
+    text_interface::add_employee_to_department(&statement, &mut department_data);
+    text_interface::add_employee_to_department(&statement2, &mut department_data);
+    text_interface::add_employee_to_department(&statement3, &mut department_data);
+    text_interface::add_employee_to_department(&incorrect_statement3, &mut department_data);
+    println!("{:?}", department_data);
+
     // part_with_data
 
     // println!("{}", text_interface::add_employee_to_department(&statement));
