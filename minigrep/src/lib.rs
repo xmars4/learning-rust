357957d85c9c080d7b4d8eb61ed2481d1b38f22d
XMars <truong.pham2@sotatek.com>
2023-08-08 23:33:46 +0700

    update using iterator

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -10,18 +10,21 @@ pub struct Config {
 
 impl Config {
     pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        let case_variable = String::from("IGNORE_CASE");
-        let ignore_case = if args.get(3..).unwrap_or_default().contains(&case_variable) {
-            true
-        } else {
-            env::var(case_variable).is_ok()
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
         };
-        Ok(Self {
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
             query,
             file_path,
             ignore_case,
@@ -45,13 +48,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
