aee2e0316520d89e8f8bb8f17a7c08b4892faac8
XMars <truong.pham2@sotatek.com>
2023-08-09 22:54:49 +0700

    update

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -55,14 +55,10 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
