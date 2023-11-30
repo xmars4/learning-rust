397de0f787ee0a3e132a5559cf63d6ec0ab0a09a
XMars <truong.pham2@sotatek.com>
2023-11-30 17:09:23 +0700

    update

--- webserver/src/lib.rs
+++ webserver/src/lib.rs
@@ -1,10 +1,16 @@
-use std::thread;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
-pub struct ThreadPoll {
+pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
-impl ThreadPoll {
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
+impl ThreadPool {
     /// Create a new ThreadPool.
     ///
     /// The size is the number of threads in the pool.
@@ -12,20 +18,27 @@ impl ThreadPoll {
     /// # Panics
     ///
     /// The `new` function will panic if the size is zero.    
-    pub fn new(size: usize) -> ThreadPoll {
+    pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPoll { workers }
+
+        ThreadPool { workers, sender }
     }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
 
@@ -35,8 +48,14 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
 
         Worker { id, thread }
     }
--- webserver/src/main.rs
+++ webserver/src/main.rs
@@ -4,11 +4,11 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
-use webserver::ThreadPoll;
+use webserver::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPoll::new(4);
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
