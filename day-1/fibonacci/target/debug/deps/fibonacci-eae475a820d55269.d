3432793779f2cd02f2fcd1eeb4bd6a167ffae3ed
XMars <truong.pham2@sotatek.com>
2023-12-20 15:41:00 +0700

    up

--- day-1/fibonacci/target/debug/deps/fibonacci-eae475a820d55269.d
+++
@@ -1,5 +0,0 @@
-/home/xmars/dev/rust/learning-rust/day-1/fibonacci/target/debug/deps/fibonacci-eae475a820d55269: src/main.rs
-
-/home/xmars/dev/rust/learning-rust/day-1/fibonacci/target/debug/deps/fibonacci-eae475a820d55269.d: src/main.rs
-
-src/main.rs:
--- day-1/fibonacci/target/debug/fibonacci.d
+++
@@ -1,1 +0,0 @@
-/home/xmars/dev/rust/learning-rust/day-1/fibonacci/target/debug/fibonacci: /home/xmars/dev/rust/learning-rust/day-1/fibonacci/src/main.rs
